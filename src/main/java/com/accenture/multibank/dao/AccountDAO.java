52a86cfd2279444890bcb86b91206b2abeabf77e
GitManCoding <tobias.schwarz@CPX-RYACT6GROUR.dir.svc.accenture.com>
2016-11-23 15:28:58 +0100

    remove import

--- src/main/java/com/accenture/multibank/dao/AccountDAO.java
+++ src/main/java/com/accenture/multibank/dao/AccountDAO.java
@@ -1,12 +1,11 @@
 package com.accenture.multibank.dao;
 
-import com.accenture.multibank.AccountReadable;
-
 /**
  * @author manuel
  * @version 11/23/16
  */
 public interface AccountDAO {
-    void save(AccountReadable account);
-    void find(int accountNumber);
+	void save(AbstractAccount account);
+
+	AbstractAccount find(int accountNumber);
 }
