39bb4ef48cbe5712bab02cba31441fb525d9c545
s.marquardt <s.marquardt@CPX-1USYWBM2HKO.dir.svc.accenture.com>
2016-11-23 14:37:47 +0100

    CreditAccount, UnbalancedCreditAccountException, AccountTest- Update

--- src/test/java/com/accenture/multibank/AccountTest.java
+++ src/test/java/com/accenture/multibank/AccountTest.java
@@ -2,7 +2,9 @@ package com.accenture.multibank;
 
 import org.junit.Test;
 
+import com.accenture.multibank.accounts.CreditAccount;
 import com.accenture.multibank.accounts.SavingAccount;
+import com.accenture.multibank.exceptions.UnbalancedCreditAccountException;
 import com.accenture.multibank.exceptions.UnbalancedSavingAccountException;
 
 public class AccountTest {
@@ -20,6 +22,24 @@ public class AccountTest {
 	@Test(expected = UnbalancedSavingAccountException.class)
 	public void testBookingVerificationShouldThrowError() {
 		SavingAccount account = new SavingAccount();
+		int amount = -120;
+
+		account.verifyBookingCondition(amount);
+
+	}
+
+	@Test
+	public void testCreditBookingVerificationShouldThrowNoError() {
+		CreditAccount account = new CreditAccount();
+		int amount = 0;
+
+		account.verifyBookingCondition(amount);
+
+	}
+
+	@Test(expected = UnbalancedCreditAccountException.class)
+	public void testCreditBookingVerificationShouldThrowError() {
+		CreditAccount account = new CreditAccount();
 		int amount = -120;
 
 		account.verifyBookingCondition(amount);
