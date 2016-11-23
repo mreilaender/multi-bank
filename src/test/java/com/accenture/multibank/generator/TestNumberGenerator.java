fd2f8a961d213e25d04d8eaadbb058648a88c32d
mreilaender <manuel.reilaender@gmail.com>
2016-11-23 12:59:36 +0100

    test SimpleAccountNumberGenerator

--- src/test/java/com/accenture/multibank/generator/TestNumberGenerator.java
+++ src/test/java/com/accenture/multibank/generator/TestNumberGenerator.java
@@ -3,30 +3,28 @@ package com.accenture.multibank.generator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 public class TestNumberGenerator {
 
-	AccountNumberGenerator generator;
+	private AccountNumberGenerator generator;
 
 	@Before
 	public void setup() {
 
-
 	}
 
 	@Test
-	public void callGeneratorAndGetAcoountNumberSimple2() {
-		generator = new AccountNumberGeneratorImplMock();
-		int accNum = generator.generateAccountNumber();
-		accNum = generator.generateAccountNumber();
-		Assert.assertEquals(2, accNum);
+	public void testSimpleAccountNumberGeneratorCount1() {
+		int startAccNr = 0, timesGenerated = 2, accNum = startAccNr;
+		generator = new SimpleAccountNumberGenerator(startAccNr);
+		for (int i = timesGenerated; i > 0;--i)
+			accNum = generator.generateAccountNumber();
+
+		Assert.assertEquals(startAccNr+timesGenerated, accNum);
 	}
 
-	@Test
-	public void callGeneratorAndGetAcoountNumberSimple1() {
-		generator = new AccountNumberGeneratorImplMock();
-		int accNum = generator.generateAccountNumber();
-
-		Assert.assertEquals(1, accNum);
-	}
 }
