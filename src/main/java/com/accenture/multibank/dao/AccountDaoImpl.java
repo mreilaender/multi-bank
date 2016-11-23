fd0943fe5745669ad6d3a7d6547a2728fe0941d9
mreilaender <manuel.reilaender@gmail.com>
2016-11-23 17:06:42 +0100

    implement AbstractDAO

--- src/main/java/com/accenture/multibank/dao/AccountDaoImpl.java
+++ src/main/java/com/accenture/multibank/dao/AccountDaoImpl.java
@@ -5,18 +5,27 @@ import java.util.Map;
 
 import com.accenture.multibank.accounts.AbstractAccount;
 
-public class AccountDaoImpl implements AccountDAO {
+public class AccountDaoImpl implements AbstractDAO<Integer, AbstractAccount> {
 
-	Map<Integer, AbstractAccount> map = new HashMap<>();
+	private Map<Integer, AbstractAccount> map = new HashMap<>();
 
-
+	@Override
 	public void save(AbstractAccount account) {
 		map.put(account.getAccountNumber(), account);
 	}
 
-	public AbstractAccount find(int accountNumber) {
-		return map.get(accountNumber);
-
+	@Override
+	public void find(Integer accountNumber) {
+		map.get(accountNumber);
 	}
 
+	@Override
+	public void update(AbstractAccount account) {
+		map.put(account.getAccountNumber(), account);
+	}
+
+	@Override
+	public void delete(Integer accountNumber) {
+		map.remove(accountNumber);
+	}
 }
