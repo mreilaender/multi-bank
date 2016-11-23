815c817cf9f090c8638d69332c7e8b83cdf11760
GitManCoding <tobias.schwarz@CPX-RYACT6GROUR.dir.svc.accenture.com>
2016-11-23 15:48:29 +0100

    package move

--- src/main/java/com/accenture/multibank/dao/AccountDAO.java
+++ src/main/java/com/accenture/multibank/dao/AccountDAO.java
@@ -1,10 +1,7 @@
 package com.accenture.multibank.dao;
 
-<<<<<<< HEAD
-=======
 import com.accenture.multibank.accounts.AccountReadable;
 
->>>>>>> branch 'DAO' of https://github.com/mreilaender/multi-bank.git
 /**
  * @author manuel
  * @version 11/23/16
