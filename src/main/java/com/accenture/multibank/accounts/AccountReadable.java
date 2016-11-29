80efc9e1d3fd366b7cf93d3b074de668068ef75e
mreilaender <manuel.reilaender@gmail.com>
2016-11-29 17:15:57 +0100

    correct spelling mistake

--- src/main/java/com/accenture/multibank/accounts/AccountReadable.java
+++ src/main/java/com/accenture/multibank/accounts/AccountReadable.java
@@ -1,9 +1,11 @@
 package com.accenture.multibank.accounts;
 
+/**
+ * @author manuel
+ * @version 11/29/16
+ */
 public interface AccountReadable {
+        public int getAccountNumber();
 
-	public int getAccountNumber();
-
-	public int getBalance();
-
+        public int getBalance();
 }
