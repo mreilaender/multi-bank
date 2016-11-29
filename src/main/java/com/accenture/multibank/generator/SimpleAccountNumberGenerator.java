8f17e413961a46010327706833a8073706fd0605
mreilaender <manuel.reilaender@gmail.com>
2016-11-29 13:46:00 +0100

    add doc and change types to Integer

--- src/main/java/com/accenture/multibank/generator/SimpleAccountNumberGenerator.java
+++ src/main/java/com/accenture/multibank/generator/SimpleAccountNumberGenerator.java
@@ -5,14 +5,18 @@ package com.accenture.multibank.generator;
  * @version 11/23/16
  */
 public class SimpleAccountNumberGenerator implements AccountNumberGenerator {
-	private int accountNumber = 0;
+	private Integer accountNumber = 0;
 
-    public SimpleAccountNumberGenerator(int start) {
+    /**
+     * Constructor
+     * @param start This is where the generator starts to count
+     */
+    public SimpleAccountNumberGenerator(Integer start) {
         this.accountNumber = start;
     }
 
     /**
-     * Increases the accountNumber by 1 and returns it
+     * Increase the accountNumber by 1 and return it
      * @return accountNumber increased by 1
      */
     public Integer generateAccountNumber() {
