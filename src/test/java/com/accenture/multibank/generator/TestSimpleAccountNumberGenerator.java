a5be22b1601140601a2399c61d849e87aeac3b4a
mreilaender <manuel.reilaender@gmail.com>
2016-11-29 13:46:24 +0100

    add test for negative and null values

---
+++ src/test/java/com/accenture/multibank/generator/TestSimpleAccountNumberGenerator.java
@@ -0,0 +1,37 @@
+package com.accenture.multibank.generator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSimpleAccountNumberGenerator {
+
+	private AccountNumberGenerator generator;
+
+	@Test
+	public void generateNumber10Times() {
+		int startAccNr = 0, timesGenerated = 10, accNum = startAccNr;
+		generator = new SimpleAccountNumberGenerator(startAccNr);
+		for (int i = timesGenerated; i > 0;--i)
+			accNum = generator.generateAccountNumber();
+
+		Assert.assertEquals(startAccNr+timesGenerated, accNum);
+	}
+
+	@Test
+	public void generateNumber10TimesWithNegativeStartValue() {
+		int startAccNr = -10, timesGenerated = 10, accNum = startAccNr;
+		generator = new SimpleAccountNumberGenerator(startAccNr);
+		for (int i = timesGenerated; i > 0;--i)
+			accNum = generator.generateAccountNumber();
+
+		Assert.assertEquals(startAccNr+timesGenerated, accNum);
+	}
+
+    @SuppressWarnings("null")
+	@Test(expected = NullPointerException.class)
+    public void generateNumberWithNullStartValue() {
+		Integer startAccNr = null, timesGenerated = 10, accNum = startAccNr;
+		generator = new SimpleAccountNumberGenerator(startAccNr);
+		Integer expected = generator.generateAccountNumber();
+	}
+}
