c28674b4045dc0a625ca096af9c52936c877e9d3
mreilaender <manuel.reilaender@gmail.com>
2016-12-20 09:19:25 +0100

    add auto wire to all fields

--- src/main/java/com/accenture/multibank/bank/RaiffeisenBank.java
+++ src/main/java/com/accenture/multibank/bank/RaiffeisenBank.java
@@ -3,22 +3,35 @@ package com.accenture.multibank.bank;
 import com.accenture.multibank.accounts.AccountModifiable;
 import com.accenture.multibank.accounts.AccountType;
 import com.accenture.multibank.dao.AbstractDAO;
+import com.accenture.multibank.entities.Account;
 import com.accenture.multibank.entities.AccountDAO;
 import com.accenture.multibank.exceptions.AccountNotFoundException;
 import com.accenture.multibank.factory.AccountFactory;
 import com.accenture.multibank.generator.AccountNumberGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author manuel
  * @version 11/29/16
  */
+@Service
 public class RaiffeisenBank implements Bank {
+    private final CrudRepository<Account, Integer> test;
+    private final AccountDAO accountDAO2;
+    private final AbstractDAO<java.lang.Integer, AccountModifiable> accountDAO;
+    private final AccountNumberGenerator accountNumberGenerator;
+    private final AccountFactory accountFactory;
+
     @Autowired
-    private AccountDAO accountDAO2;
-    private AbstractDAO<java.lang.Integer, AccountModifiable> accountDAO;
-    private AccountNumberGenerator accountNumberGenerator;
-    private AccountFactory accountFactory;
+    public RaiffeisenBank(AccountDAO accountDAO2, AccountNumberGenerator accountNumberGenerator, AccountFactory accountFactory, AbstractDAO<Integer, AccountModifiable> accountDAO, CrudRepository<Account, Integer> test) {
+        this.accountDAO2 = accountDAO2;
+        this.accountNumberGenerator = accountNumberGenerator;
+        this.accountFactory = accountFactory;
+        this.accountDAO = accountDAO;
+        this.test = test;
+    }
 
     @Override
     public boolean withdraw(Integer accNr, int amount) {
@@ -54,24 +67,7 @@ public class RaiffeisenBank implements Bank {
     public Integer createAccount(AccountType type, int balance) {
         AccountModifiable newAccount = accountFactory.createAccount(accountNumberGenerator, type, balance);
         accountDAO.save(newAccount);
-        if (accountDAO2 != null)
-            System.out.println("not null!!!!");
+        accountDAO2.equals(null);
         return newAccount.getAccountNumber();
-    }
-
-    public void setAccountDAO(AbstractDAO<java.lang.Integer, AccountModifiable> accountDAO) {
-        this.accountDAO = accountDAO;
-    }
-
-    public void setAccountNumberGenerator(AccountNumberGenerator accountNumberGenerator) {
-        this.accountNumberGenerator = accountNumberGenerator;
-    }
-
-    public void setAccountFactory(AccountFactory accountFactory) {
-        this.accountFactory = accountFactory;
-    }
-
-    public void setAccountDAO2(AccountDAO accountDAO2) {
-        this.accountDAO2 = accountDAO2;
     }
 }
--- src/test/java/com/accenture/multibank/bank/RaiffeisenBankTest.java
+++ src/test/java/com/accenture/multibank/bank/RaiffeisenBankTest.java
@@ -1,11 +1,8 @@
 package com.accenture.multibank.bank;
 
-import com.accenture.multibank.accounts.AccountType;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -13,34 +10,29 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @version 11/30/16
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/beans.xml", "/daos.xml", "/factories.xml", "/generators.xml"})
 public class RaiffeisenBankTest {
-    private Bank raiffeisen;
+    private Bank raiffeisenBank;
     private int accNr1, accNr2, balance;
 
     @Before
     public void setUp() throws Exception {
         balance = 1000;
-        accNr1 = raiffeisen.createAccount(AccountType.SAVING, balance);
-        accNr2 = raiffeisen.createAccount(AccountType.SAVING, balance);
+        accNr1 = raiffeisenBank.createAccount(AccountType.SAVING, balance);
+        accNr2 = raiffeisenBank.createAccount(AccountType.SAVING, balance);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void withdrawWithNegativeValue() throws Exception {
-        raiffeisen.withdraw(accNr1, -100);
+        raiffeisenBank.withdraw(accNr1, -100);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void depositWithNegativeValue() throws Exception {
-        raiffeisen.withdraw(accNr1, -100);
+        raiffeisenBank.withdraw(accNr1, -100);
     }
 
     @Test
     public void name() throws Exception {
 
-    }
-
-    public void setRaiffeisen(Bank raiffeisen) {
-        this.raiffeisen = raiffeisen;
     }
 }
