ba5822fea07180b3f49ee3368eef1b6310c18aa4
mreilaender <manuel.reilaender@gmail.com>
2016-12-20 16:14:17 +0100

    add service annotation

--- src/main/java/com/accenture/multibank/bank/Bank.java
+++ src/main/java/com/accenture/multibank/bank/Bank.java
@@ -1,11 +1,13 @@
 package com.accenture.multibank.bank;
 
 import com.accenture.multibank.accounts.AccountType;
+import org.springframework.stereotype.Service;
 
 /**
  * @author manuel
  * @version 11/29/16
  */
+@Service
 public interface Bank {
     // TODO: Generic bank because the account number may not be represented by an integer
     boolean withdraw(Integer accountNumber, int amount);
