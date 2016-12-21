9188cf1131709df4109dac80ae668bb98fdd4793
britt-marie.meiners <britt-marie.meiners@CPX-PXMTZ1YSD4N.dir.svc.accenture.com>
2016-12-21 14:49:52 +0100

    NEW POM.xlm

--- src/test/java/controller/BankControllerTest.java
+++ src/test/java/controller/BankControllerTest.java
@@ -1,38 +1,30 @@
 package controller;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.accenture.multibank.Main;
 import com.accenture.multibank.accounts.AccountType;
 import com.accenture.multibank.bank.Bank;
-import com.accenture.multibank.bank.RaiffeisenBank;
 import com.accenture.multibank.controller.BankController;
-import com.accenture.multibank.dao.HashMapAccountDAO;
 import com.accenture.multibank.entities.Transaction;
-import com.accenture.multibank.factory.SimpleAccountFactory;
-import com.accenture.multibank.generator.SimpleAccountNumberGenerator;
 
-@SpringBootTest
+@SpringBootTest(classes = { Main.class })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BankControllerTest {
 
-
+	@MockBean
 	Bank bank;
 	@Autowired
 	BankController bankController;
 
-	@Before
-	public void testBefore() {
-		bank = new RaiffeisenBank(new SimpleAccountNumberGenerator(), new SimpleAccountFactory(),
-				new HashMapAccountDAO());
-		bankController = new BankController(bank);
-
-	}
 
 	@Test
 	public void testcreateAccount() throws Exception {
@@ -42,6 +34,8 @@ public class BankControllerTest {
 
 	@Test
 	public void testbook() throws Exception {
+		when(bank.createAccount(AccountType.SAVING, 200)).thenReturn(1234);
+
 		int Accnr1 = bank.createAccount(AccountType.SAVING, 200);
 		String extreneAccNr = "Y" + Accnr1;
 		Transaction transaction = new Transaction(extreneAccNr, null, 100);
