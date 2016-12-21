3dfc486a1178b7263e670aa054be411cd217950a
mreilaender <manuel.reilaender@gmail.com>
2016-12-21 15:25:36 +0100

    merge

---
+++ src/test/java/com/accenture/multibank/controller/BankControllerTest.java
@@ -0,0 +1,46 @@
+package com.accenture.multibank.controller;
+
+import com.accenture.multibank.Main;
+import com.accenture.multibank.accounts.AccountType;
+import com.accenture.multibank.bank.Bank;
+import com.accenture.multibank.entities.Transaction;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = { Main.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BankControllerTest {
+
+	@MockBean
+	Bank bank;
+
+	@Autowired
+	BankController bankController;
+
+
+	@Test
+	public void testcreateAccount() throws Exception {
+		String actuell = bankController.createAccount(AccountType.SAVING);
+		Assert.assertNotNull(actuell);
+	}
+
+	@Test
+	public void testbook() throws Exception {
+		when(bank.createAccount(AccountType.SAVING, 200)).thenReturn(1234);
+
+		int Accnr1 = bank.createAccount(AccountType.SAVING, 200);
+		String extreneAccNr = "Y" + Accnr1;
+		Transaction transaction = new Transaction(extreneAccNr, null, 100);
+
+		bankController.book(transaction);
+	}
+
+
+}
---
+++ src/test/java/com/accenture/multibank/controller/BankControllerWithAspectTest.java
@@ -0,0 +1,24 @@
+package com.accenture.multibank.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.accenture.multibank.Main;
+
+@SpringBootTest(classes = { Main.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class BankControllerWithAspectTest {
+
+	@Autowired
+	BankController bankController;
+
+	@Test(expected = Exception.class)
+	public void testName() throws Exception {
+		bankController.book(null);
+	}
+
+}
