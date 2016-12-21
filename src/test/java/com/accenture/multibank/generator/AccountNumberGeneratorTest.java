9a8d1126331b403385652958d63a683566325779
s.marquardt <s.marquardt@CPX-1USYWBM2HKO.dir.svc.accenture.com>
2016-12-21 17:28:20 +0100

    accountNumberGenratorTest fixed so it can be executed

--- src/test/java/com/accenture/multibank/generator/AccountNumberGeneratorTest.java
+++ src/test/java/com/accenture/multibank/generator/AccountNumberGeneratorTest.java
@@ -1,13 +1,17 @@
 package com.accenture.multibank.generator;
 
-import com.accenture.multibank.entities.AccountNumbers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.accenture.multibank.Main;
+import com.accenture.multibank.entities.AccountNumbers;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { Main.class })
 public class AccountNumberGeneratorTest {
 
 	@Autowired
